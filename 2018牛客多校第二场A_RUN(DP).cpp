4c57b8c1ac09a8311c0db6925b7e422e8f95531b
Aoengi <37430059+Aoengi@users.noreply.github.com>
2018-08-28 11:24:29 +0800

    Update 2018牛客多校第二场A_RUN(DP).cpp

--- 2018牛客多校第二场A_RUN(DP).cpp
+++ 2018牛客多校第二场A_RUN(DP).cpp
@@ -1,4 +1,12 @@
-﻿#include<bits/stdc++.h>
+/*
+	题目链接：https://www.nowcoder.com/acm/contest/140/A
+	题意：一个人可以选择一次走1m或者选择一次跑k m，问从L跑到R有多少种方法；
+	思路：dp推导，dp[i][0]表示走i米的方法数，dp[i][1]表示跑到第i米所表示的方法数，
+	那么到第i米走着来的所用的方法数可以由第i-1米走着来的和跑着来的方法数得到，
+	那么第i米跑着来的方法数可以由第i-k米的方法数得到，这样就得到一个递推式：
+	dp[i][0]=dp[i-1][0]+dp[i-1][1],dp[i][1]=dp[i-k];
+*/
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 const int mod=1e9+7;
